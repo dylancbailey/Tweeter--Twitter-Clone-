a4024662819095323bc2665e255393457b972bef
Dylan Bailey <wearesleepless@gmail.com>
2023-09-21 12:07:52 -0400

    Checking UUID for likes

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ tweetBtn.addEventListener("click", function() {
     console.log(tweetInput.value)
 })
 
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+
+function handleLikeClick(tweetID) {
+    console.log(tweetID)
+}
+
 function getFeedHtml() {
     let feedHtml = ``
 
@@ -20,15 +30,15 @@ function getFeedHtml() {
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
-                            <i class="fa-regular fa-comment-dots"></i>
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-heart"></i>
+                            <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-repeat"></i>
+                            <i class="fa-solid fa-repeat" data-retweet="${tweet.uuid}"></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
