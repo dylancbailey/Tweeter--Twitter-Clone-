509ca2592fd41a1a57e7f721517411c09b107fe4
Dylan Bailey <wearesleepless@gmail.com>
2023-09-22 10:55:31 -0400

    Render tweet to page

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { tweetsData } from "./data.js"
-
-const tweetInput = document.getElementById("tweet-input")
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 document.addEventListener("click", function(e) {
     if (e.target.dataset.like) {
@@ -48,7 +47,22 @@ function handleReplyClick(replyID) {
 }
 
 function handleTweetBtnClick() {
-    console.log(tweetInput.value)
+    const tweetInput = document.getElementById("tweet-input")
+    if (tweetInput.value) {
+        tweetsData.unshift({
+            handle: `@Beebo`,
+            profilePic: `images/scrimbalogo.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4(),
+        }) 
+        tweetInput.value = ""
+        render()
+    }
 }
 
 function getFeedHtml() {
